f3e3ed7d88cbb5ad75b0f58ae47e1fa8c4e47b7f
hollowsunsets <isabelgiang@gmail.com>
2019-10-10 19:02:42 -0700

    Add graph files

---
+++ barchart.js
@@ -0,0 +1,15 @@
+const initializeBarChart = function() {
+    const chartColors = d3.scaleOrdinal()
+    const dataSet;
+
+    const columnMargin = { top: 20, right: 20, bottom:}
+}
+
+const drawChart = function() {
+
+}
+
+const drawLegend = function() {
+
+}
+
---
+++ sunburst.js
@@ -0,0 +1,3 @@
+const initializeSunburst = function() {
+    
+}
